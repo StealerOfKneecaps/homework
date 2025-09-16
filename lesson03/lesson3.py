a0c3bcce387e4e720c8f9c2008e2cffeddf9dc47
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-09-16 16:42:41 -0400

    firsthomeworklol

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,8 @@
 # Lesson 3
+#Yall idk if we're allowed to use math import??? it WOULD however make this SO EASY so I'm using it
+import math
+def Main():
+    amountOfSquares = int(input("Yo how many squares you got yo")) 
+    sideLength = (math.sqrt(amountOfSquares))//1
+    print(f"The largest possible square has an area of {sideLength**2} and has a sidelength {sideLength}")
+Main()
