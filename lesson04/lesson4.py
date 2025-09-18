dadc6736806f511b12a500bdb33a570a7ee75474
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-09-18 16:33:06 -0400

    uhhh finished these 2 days

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,11 @@
-# Lesson 4
+import math
+# Lesson 4 yall apparently .length is len in python idk how I feel about taht bro
+lengthOne = input("How long is the first section of fence?\n")
+lengthTwo = input("How long is the second section of fence?\n")
+lengthThr = input("How long is the third section of fence?\n")
+totalLength = len(lengthOne)+len(lengthTwo)+len(lengthThr)
+timesToBuyPain = math.ceil(totalLength/12)
+cost = (round((timesToBuyPain*14.95)*100))/100
+#guys this is just rounding it to 2 decimal places don't worry guys
+bucketsLeft = timesToBuyPain*12-totalLength
+print(f"you need {totalLength} buckets.\n you would have {bucketsLeft} buckets left.\n it would cost ${cost}.")
--- lesson05/lesson5.py
+++ lesson05/lesson5.py
@@ -1,1 +1,8 @@
 # Lesson 5
+#shouldn't be too bad. I think count would be cool
+cookieSellBuy = input("Yo do the bcbcbc thing ").lower().strip()
+bigCookie = cookieSellBuy.count("b")
+regCookie = cookieSellBuy.count("c")
+bigCookieCost = bigCookie*1.25
+regCookieCost = regCookie*0.75
+print(f"you sold {bigCookie+regCookie} cookies. You made a profit of ${bigCookieCost+regCookieCost}. You made a total of ${bigCookie*2+regCookie*1.25}.")
