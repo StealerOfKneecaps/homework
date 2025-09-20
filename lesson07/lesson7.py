47dd855fb56378b8dda3269240a5967f04f330db
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-09-19 23:06:53 -0400

    homework les7

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,10 @@
-# Lesson 7
+# Lesson 
+#This'll be a pain
+import random
+diffCheck = int(input("Yo what's the dc yo"))
+diceRoll = random.randrange(1,21)
+print (f"The roll was {diceRoll}" )
+if diffCheck>= diceRoll:
+    print ("you FAILED LMAOOOOO")
+else:
+    print ("eh you won")
