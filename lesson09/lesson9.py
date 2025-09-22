d980a5df6231016c086568b211dd0823c84116cd
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-09-22 17:04:09 -0400

    fukin roshambo

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,19 @@
-# Lesson 9
+import random
+# Lesson 
+# yo it's roshambo
+dictOfWhoBeatsWho = {
+    "r":"s",
+    "p":"r",
+    "s":"p"
+}
+wannaContinue = "y"
+while (wannaContinue == "y"):
+    aiChoice = random.choice(["r", "p", "s"])
+    playerChoice = input("yo bro roshambo time what do you chcoose r p or s").lower()
+    if playerChoice == aiChoice:
+        print ("draw.")
+    elif dictOfWhoBeatsWho[aiChoice] == playerChoice:
+        print ("fricken loser lmao")
+    else:
+        print ("WOW you WON you GENIUS")
+    wannaContinue = input("Bro wanna go again y/n")
