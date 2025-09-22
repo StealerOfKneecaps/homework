cde53c180f9cedbcfb954396e78b0a48d10227e1
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-09-22 16:52:47 -0400

    this was in the java questions lmao

--- lesson08/lesson8.py
+++ lesson08/lesson8.py
@@ -1,1 +1,14 @@
 # Lesson 8
+#remember doing this is Java lol    
+wCounter = 0
+for _ in range (0,6):
+    wOrL = input("Hey yo (all lower case) bruh w or l ")
+    if wOrL=="w" or wOrL=="W":
+        print("broo that's crazy bro")
+        wCounter+=1
+if wCounter == 6 or wCounter == 5:
+    print("group 1")
+elif wCounter == 4 or wCounter == 3:
+    print("group 2")
+elif wCounter == 2 or wCounter ==1:
+    print("group 3")
