0f297d7ee3b2c4bc1b0fca9f60ef6b6242c139d2
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-09-23 10:34:10 -0400

    easy question

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,6 @@
 # Lesson 10
+teleNumber = input("What is the number that you want to know if telemarketer or no? last 4 digits\n")
+if (teleNumber[0]=="8" or teleNumber[0]=="9") and (teleNumber[1]==teleNumber[2]) and (teleNumber[3]=="8" or teleNumber[3]=="9"):
+    print("yea it's a marketer lmao block their ah")
+else:
+    print("nah it's not a marketer")
