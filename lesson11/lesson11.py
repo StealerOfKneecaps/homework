15804c8a90170e7a2ff7df24012d6609648b9766
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-09-23 10:49:28 -0400

    idk where I had to use map?

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,15 @@
 # Lesson 11
+xCoordYCoord = input("What are the coordinates")
+(x, y) = xCoordYCoord.split(",") #Whenever there's a comma, it splits it
+#you are splitting it into variables x and y
+
+if int(x)>0 and int(y)>0:
+    print ("q1")
+elif int(x)>0 and int(y)<0:
+    print("q4")
+elif int(x)<0 and int(y)>0:
+    print ("q2")
+elif int(x)<0 and int(y)<0:
+    print ("q3")
+else:
+    print ("it's on one of the axes")
