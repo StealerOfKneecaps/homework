4ebc753a196d822d4f456e7350dc399f77d0d1d0
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-09-28 17:59:55 -0400

    few hw questions

--- lesson12/lesson12.py
+++ lesson12/lesson12.py
@@ -1,1 +1,14 @@
-# Lesson 12
+# Lesson 
+#The legendary triangle question
+print("Hi! I will determine if your triangle is equil., scal., or iso.")
+angleA = int(input("What is your first angle\n"))
+angleB = int(input("What is your second angle\n"))
+angleC = int(input("What is yoru third angle\n"))
+if angleA+angleB+angleC!= 180:
+    print("Nice try sucker ")
+elif angleA==angleB==angleC:
+    print("Equilateral!")
+elif angleA!=angleB!=angleC:
+    print("Isosceles!")
+else:
+    print("Scalene!")
--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,14 @@
 # Lesson 13
+monthNumber =  int(input("What's the month? number"))
+if monthNumber>2:
+    print("After feb18")
+elif monthNumber<2:
+    print("Before feb18")
+else:
+    dayNumber = int(input("What's the day?"))
+    if dayNumber>18:
+        print("After 18th")
+    elif dayNumber<18:
+        print("Before 18th")
+    else:
+        print("On 18th")
