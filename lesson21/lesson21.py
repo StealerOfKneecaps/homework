eacba42863edfcae4b5d832c79776845e73fc8a3
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-10-10 10:26:35 -0400

    holy shit finally

--- lesson21/lesson21.py
+++ lesson21/lesson21.py
@@ -1,1 +1,14 @@
 # Lesson 21
+def greatestIn(theNumber):
+    curretnGreat = 0
+    biggestNum = 0
+    for i in range(1, theNumber+1):
+        factNum = 0
+        for j in range (1, i+1):
+            if i%j==0:
+                factNum+=1
+        if factNum>=curretnGreat:
+            curretnGreat = factNum
+            biggestNum = i
+    return biggestNum
+print (greatestIn(52))
