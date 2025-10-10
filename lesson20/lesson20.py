8db128ea55553dc1100b9a5d5a55cb0d1175105d
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-10-10 09:51:44 -0400

    a single hw quest

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,1 +1,10 @@
 # Lesson 20
+def perf_numbrero(num):
+    sum = 0
+    for i in range (1, int(num//2)+1):
+        if num%i==0:
+            sum+=i
+    return sum==num
+for i in range (0,10000):
+    if perf_numbrero(i):
+        print(i)
