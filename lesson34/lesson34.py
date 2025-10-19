d2075650dc57359d1cb5616d578500b39a3279d3
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-10-19 19:55:22 -0400

    TRUE!!!

--- lesson34/lesson34.py
+++ lesson34/lesson34.py
@@ -1,1 +1,7 @@
-# Lesson 34
+import random
+def splitter(magicThing):
+    return magicThing.split(",",-1)
+def randomRanger(magicThing2,magicThing3):
+    return random.randrange(magicThing2, magicThing3+1)
+print (splitter("reimu,is,the,best"))
+print (randomRanger(1,5))
