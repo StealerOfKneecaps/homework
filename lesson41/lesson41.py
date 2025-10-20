f4d5e00adad0656b65d535500c3dead02cf2ec55
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-10-20 16:33:31 -0400

    I can't be bothered to see if this wokrs

--- lesson41/lesson41.py
+++ lesson41/lesson41.py
@@ -1,1 +1,14 @@
 # Lesson 41
+dictPoints = {
+    "a": 1, "e": 1, "i": 1, "l": 1, "n": 1, "o": 1, "r": 1, "s": 1, "t": 1, "u": 1,
+    "d": 2, "g": 2,
+    "b": 3, "c": 3, "m": 3, "p": 3,
+    "f": 4, "h": 4, "v": 4, "w": 4, "y": 4,
+    "k": 5,
+    "x": 8, "j": 8,
+    "q": 10, "z": 10
+}
+sum = 0
+for chara in "skibidirizz":
+    sum+= dictPoints[chara]
+print(sum)
