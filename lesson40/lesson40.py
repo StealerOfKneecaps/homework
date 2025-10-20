b328fe62696559f132751da45184628e4ae1b4c1
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-10-20 16:16:08 -0400

    only 98% sure this works

--- lesson40/lesson40.py
+++ lesson40/lesson40.py
@@ -1,1 +1,9 @@
 # Lesson 40
+def is_prime(lordofthemysteriesiskindabad):
+    for i in range (2, int(lordofthemysteriesiskindabad**0.5//1+1)):
+        if lordofthemysteriesiskindabad%i==0:
+            return False
+    return True
+for i in range (1, 10001):
+    if is_prime(i):
+        print(i)
