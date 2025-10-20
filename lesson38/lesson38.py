8e0849d31ca946c5c543b8e1fbdf3900eceb226a
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-10-20 16:05:34 -0400

    this feels increidlby bad

--- lesson38/lesson38.py
+++ lesson38/lesson38.py
@@ -1,1 +1,11 @@
 # Lesson 38
+def is_palin(palArg):
+    if str(palArg)[:]== str(palArg)[::-1]:
+        return True
+    return False
+listOfPalins = []
+for i in range (100, 1000):
+    for j in range (100, 1000):
+        if is_palin(i*j):
+            listOfPalins.append(i*j)
+print (max(listOfPalins))
