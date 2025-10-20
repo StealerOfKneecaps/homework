29f5c600116d4908d7f9157bd1d00f6f26585e98
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-10-20 10:40:35 -0400

    holy fucking shit bruh finally ts pmo heartbreak emoji

--- lesson37/lesson37.py
+++ lesson37/lesson37.py
@@ -1,1 +1,19 @@
 # Lesson 37
+def string_zipper(zipped):
+    returner = zipped[0]
+    prevChar = zipped[0]
+    counter=0
+    totalCount = 0
+    for char in zipped:
+        if char==prevChar:
+            counter+=1
+            totalCount+=1
+        else:
+            totalCount+=1
+            prevChar = zipped[totalCount]
+            returner += str(counter)
+            counter = 1
+            returner += char
+    returner+=str(counter)
+    return returner
+print(string_zipper("aaabbbbcccccdd"))
