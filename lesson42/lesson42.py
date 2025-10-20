9443db7910c7ea3a53ffafc23da50d5b4f036d59
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-10-20 16:46:48 -0400

    dasmn I know what an enumerate is now

--- lesson42/lesson42.py
+++ lesson42/lesson42.py
@@ -1,1 +1,7 @@
 # Lesson 42
+def is_sum_of(list,target):
+    for i, value in enumerate(list):
+        newerTarg = target-value
+        if newerTarg in list[i+1:]:
+            return True
+    return False
