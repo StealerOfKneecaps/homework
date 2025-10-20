d8eaaabcc44108bbacd804778f2aa1d07f432ebe
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-10-19 20:06:48 -0400

    what is the 'reimu touhou project' I keep on seeing

--- lesson35/lesson35.py
+++ lesson35/lesson35.py
@@ -1,1 +1,8 @@
 # Lesson 35
+def dupeRemover(magicThing):
+    newList = []
+    for thing in magicThing:
+        if thing not in newList:
+            newList.append(thing)
+    return newList
+print (dupeRemover(["reimu", "reimu", "reimu", "sakuya","marisa","reimu","sanae"])) 
