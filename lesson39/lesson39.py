d848dd26518088faf9ac71de852fa90b1f4e4fda
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-10-20 16:12:24 -0400

    uhh I'm only 99% sure this works

--- lesson39/lesson39.py
+++ lesson39/lesson39.py
@@ -1,1 +1,7 @@
 # Lesson 39
+def common_finder(first, sec):
+    for possFact in range (min(first,sec), 0, -1):
+        if max(first,sec)%possFact==0 and min(first,sec)%possFact==0:
+            return possFact
+    return "None"
+print (common_finder(100, 98))
