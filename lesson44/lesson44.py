8a776e8ca8f5a201d7915bb706e57cad3e78b6a5
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-10-20 17:03:23 -0400

    damn I didn'tknow you could just do that

--- lesson44/lesson44.py
+++ lesson44/lesson44.py
@@ -1,1 +1,10 @@
 # Lesson 44
+def dict_maker(aString):
+    sortedString = sorted(aString.lower())
+    answer = {}
+    for chara in sortedString:
+        if chara not in answer:
+            answer[character] = 1
+        else:
+            answer[character] += 1
+    return answer
