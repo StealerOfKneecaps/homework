c0d3086aa0bc8a0f8bd50b6934aa72c6b974d78e
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-10-20 10:04:57 -0400

    day 2

--- lesson36/lesson36.py
+++ lesson36/lesson36.py
@@ -1,1 +1,18 @@
 # Lesson 36
+def is_prime(argPrime):
+    for i in range (2, int(argPrime**0.5//1+1)):
+        if argPrime%i==0:
+            return False
+    return True
+
+for j in range (1, 10001):
+    if is_prime(j):
+        print(j)
+
+argFact = 25
+for i in range (1, int(argFact**0.5//1+1)):
+    if argFact%i==0 and int(argFact/i) != int(i):
+        print(i)
+        print(argFact/i)         
+    elif argFact%i==0:
+        print(i)
