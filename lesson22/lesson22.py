d398e62692aa81f645ae9bdf64313ecac00c90ac
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-10-21 17:00:41 -0400

    first half of deletesued questions

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,11 @@
-# Lesson 22
+# Lesson 
+#ok I'm gonna redo these beacuse it's like 7 quesions total
+
+def fibo_finder(fiboArger):
+    if fiboArger ==0:
+        return 0
+    elif fiboArger==1:
+        return 1
+    else:
+        return fibo_finder(fiboArger-1) + fibo_finder(fiboArger-2)
+print (fibo_finder(25))
--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,12 @@
 # Lesson 23
+sum = 0
+count = 0
+while True:
+    
+    res = input("hey brosephine gimme a number (if it's \"stop\" I'll stop)")
+    if res=="stop":
+        break
+    else:
+        sum+=int(res)
+        count+=1
+print (sum/count)
