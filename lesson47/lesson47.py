d8c1e14ef562ee7a9f1341419f58cdbd3db538b2
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-10-21 16:50:03 -0400

    well well well

--- lesson47/lesson47.py
+++ lesson47/lesson47.py
@@ -1,1 +1,8 @@
-# Lesson 47
+# Lesson 
+#we got an easy one after that goddayum one last time
+def factorail_but_addition(numberToBeFactorialed):
+    sum = 0
+    for i in range (numberToBeFactorialed, 0, -1):
+        sum += i
+    return sum
+print (factorail_but_addition(1000000))#this is pretty fast actually
